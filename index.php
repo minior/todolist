921ce7424db56fe14970bf25e123c2cc5c169299
minior <jjerel.koh@gmail.com>
2024-05-06 17:03:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
-
 <!DOCTYPE html>
 <html>
+<?php
+session_start(); 
+//defining variables: username, 
+if (isset($_SESSION['username'])) {
+    $username=$_SESSION['username'];
+} else {
+    $username="";
+}
+?>
     <head>
+        <title>minior's To-Do List</title>
+        <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width">
+        <!-- css style sheet -->
+    </head>
+<body>
+    <?= '<h1> Welcome '. $username.'! </h1>' ?>
+    <p> Start your productivity session, or <a href="login.php">Log In</a> to access your saved lists </p>
+</body>
+</html>
