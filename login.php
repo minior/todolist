fbda01861d47847f25582ca3e1deaea1a666300f
minior <jjerel.koh@gmail.com>
2024-05-13 16:12:41 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,14 @@
 <?php
-session_start()
+session_start();
+//if logged in, redir to index
+if(isset($_SESSION['username'])) {
+    header('location: index.php');
+    return;
+}
+
+//insert user info
+
+//edit / delete / create user
 <!DOCTYPE html>
 <html>
 <head>
